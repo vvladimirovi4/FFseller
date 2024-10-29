5c075b3ec15277b8325a9b73984649b96b14e502
VladimirGarmanov <v.garmanov@sch-int.ru>
2024-10-29 15:55:02 +0300

    tgk

--- resend/main_telethon.py
+++ resend/main_telethon.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-# Замените на свои значения API ID и API Hash
+# Указываем API ID и API Hash
 api_id = 21564722
 api_hash = 'a37810f1f9432966784ac36e1df673bd'
 
@@ -9,7 +9,7 @@ api_hash = 'a37810f1f9432966784ac36e1df673bd'
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 # Список ключевых слов для фильтрации сообщений
-keywords = ["фулфилмент", "прайс", "расценки", "доставка"]  # Ваши ключевые слова
+keywords = ["фулфилмент", "прайс", "расценки", "доставка"]
 
 # Функция для поиска канала, в котором аккаунт является администратором
 async def get_admin_channel(client: Client):
@@ -21,21 +21,21 @@ async def get_admin_channel(client: Client):
 # Переменная для хранения ID канала администратора
 admin_channel_id = None
 
-# Обработчик, который запускается при старте бота
-@app.on_message(filters.me & filters.command("start", prefixes="."))
-async def start(client, message):
+# Инициализация ID канала перед обработкой сообщений
+@app.on_message()
+async def initialize_channel(client: Client, message: Message):
     global admin_channel_id
-    admin_channel_id = await get_admin_channel(client)
-    if admin_channel_id:
-        await message.reply(f"Найден канал: {admin_channel_id}. Сообщения будут пересылаться при наличии ключевых слов.")
-    else:
-        await message.reply("Канал, где вы являетесь администратором, не найден.")
+    if admin_channel_id is None:  # Канал еще не инициализирован
+        admin_channel_id = await get_admin_channel(client)
+        if admin_channel_id is None:
+            print("Канал, где вы являетесь администратором, не найден.")
+            return
 
 # Обработчик сообщений для пересылки в канал при наличии ключевых слов
-@app.on_message(filters.chat)
+@app.on_message()
 async def forward_to_channel(client, message: Message):
     global admin_channel_id
-    if admin_channel_id:
+    if admin_channel_id and message.text:
         # Проверяем, есть ли ключевые слова в тексте сообщения
         if any(keyword.lower() in message.text.lower() for keyword in keywords):
             await message.forward(admin_channel_id)
